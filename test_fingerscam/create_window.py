2fcd92504c535d5e9df20d4ee59fef6efea82134
felipe <luizfreitas2712@hotmail.com>
2023-10-03 16:41:28 -0400

    teste 4

--- test_fingerscam/create_window.py
+++ test_fingerscam/create_window.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import time
+
+
 import os
 
 fotoApp = os.path.dirname(__file__)
@@ -10,8 +11,10 @@ class Aplication():
         self.root = Tk()
         self.config_tela()
         self.frames()
+        self.imagem_logo()
+
+
         self.root.mainloop()
-        self.botao()
         
     def config_tela(self):
         self.root.geometry("900x700")
@@ -26,13 +29,17 @@ class Aplication():
         self.frame_foto.place(relx= 0.44 ,rely= 0.3, width=250, height=300)
 
         self.frame_logo = Frame(self.root, bg="#c0c0c0")
-        self.frame_logo.place(relx= 0.41,rely= 0.01, width= 350, height= 250)
+        self.frame_logo.place(relx= 0.41,rely= 0.01, width= 500, height= 500)
         
 
-    def botoes(self):
-        botao_1 = Button(self.root, text='blablabla')
+    def imagem_logo(self):
+        self.my_image = PhotoImage(file="/home/projexln01/Desktop/leitor_digital/assets/images/Logo_projex_Branca.png")
+        self.imagemFinal = Label(self.frame_logo, image=self.my_image)
+        self.imagemFinal.place(relx=0.01, rely=0.01, width=1000, height=1000)
+
+    # def inputs(self):
+    #     self.input_1 = 
+        
     
-        
-
     
         
