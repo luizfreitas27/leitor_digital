4594b71b2350b9d38ebf4faa620b08aea678c37e
felipe <luizfreitas2712@hotmail.com>
2023-10-04 16:55:32 -0400

    teste 6

--- test_fingerscam/create_window.py
+++ test_fingerscam/create_window.py
@@ -3,6 +3,8 @@ from tkinter.font import Font
 
 
 class Aplication():
+
+    ### Initialization
     def __init__(self):
         self.root = Tk()
         self.config_tela()
@@ -14,6 +16,8 @@ class Aplication():
         self.root.mainloop()
         
     def config_tela(self):
+
+        ### Configs basicas da tela
         self.root.geometry("1920x1080")
         self.root.title('Aplication')
         self.root.configure(background="black")
@@ -24,18 +28,23 @@ class Aplication():
         ### Tela de imagem da pessoa
         self.frame_foto = Frame(self.root, bg="white")
         self.frame_foto.place(relx= 0.44 ,rely= 0.3, width=250, height=300)
-
+        
+        ### Caixa de input nome
         self.frame_input = Frame(self.root, bg="black")
         self.frame_input.place(relx= 0.44 ,rely= 0.65, width=250, height=25)
            
 
     def imagem_logo(self):
+
+        ### Logo empresa
         self.my_image = PhotoImage(file="/home/projexln01/Desktop/leitor_digital/assets/images/Logo_Projex_71x66.5.png")
         self.imagemFinal = Label(self.root, image=self.my_image, bg="black")
         self.imagemFinal.place(relx=0.45, rely=0.00, width=200, height=200)
 
     
     def input(self):
+
+        ### Input nome funcionario
         self.label_nome = Label(self.frame_input,text="Nome    ", bg="black", fg="white")
         self.label_nome.grid(column=1, row=0)
         self.input_nome = Entry(self.frame_input)
